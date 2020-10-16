441fa4c4a83e32900f66ea7395485427ecd15041
Tong Sheng Ng <tongsheng.ng@cheetahdigital.com>
2020-10-16 16:46:26 +0800

    added sample validator

--- apps/pricing-table/pages/api/pricing.ts
+++ apps/pricing-table/pages/api/pricing.ts
@@ -1,7 +1,21 @@
 import { Pricing } from '@driveflux-code-challenge/interfaces';
+import Joi from 'joi';
 import db from '../../db';
 
-export default function handler(req, res) {
+const requiredString = Joi.string().required();
+const requiredStringWithNumber = requiredString.regex(/^\d+$/);
+
+const schema = Joi.array().items(
+  Joi.object().keys({
+    id: requiredString,
+    label: requiredString,
+    lite: requiredStringWithNumber,
+    standard: requiredStringWithNumber,
+    unlimited: requiredStringWithNumber,
+  })
+);
+
+export default async function handler(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
 
@@ -13,8 +27,15 @@ export default function handler(req, res) {
 
   if (req.method === 'PUT') {
     const { pricings } = req.body;
+    const { error } = schema.validate(pricings);
+
+    if (error) {
+      res.statusCode = 400;
+      res.end(JSON.stringify(error));
+      return;
+    }
+
     db.set('pricings', pricings).write();
     res.end(JSON.stringify({ success: true }));
-    return;
   }
 }
